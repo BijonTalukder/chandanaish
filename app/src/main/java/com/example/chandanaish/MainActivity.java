52f96856e04cc9e2cbb98512ed6e53ab96d91287
BijonTalukder <81519797+BijonTalukder@users.noreply.github.com>
2023-02-10 23:11:04 +0600

    updated

--- app/src/main/java/com/example/chandanaish/MainActivity.java
+++ app/src/main/java/com/example/chandanaish/MainActivity.java
@@ -3,12 +3,25 @@ package com.example.chandanaish;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    ImageView temparature;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        temparature = findViewById(R.id.temparature);
+
+        temparature.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                web.url="https://justweather.org/Bangladesh/Chittagong/Chittagong/Chandanaish/Hourly/";
+
+
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/chandanaish/web.java
@@ -0,0 +1,25 @@
+package com.example.chandanaish;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class web extends AppCompatActivity {
+    public  static String url;
+    WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_web);
+        webView= findViewById(R.id.webloader);
+//        webView.getSettings().setUserAgentString(USER_AGENT_);
+        webView.getSettings().setLoadsImagesAutomatically(true);
+        webView.getSettings().setAppCacheEnabled(true);
+        webView.getSettings().setAllowFileAccess(true);
+
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl(url);
+    }
+}
