8853dfcefeabfb98ebe31a6f8d091c2d2dfa8de0
BijonTalukder <81519797+BijonTalukder@users.noreply.github.com>
2023-02-16 00:22:17 +0600

    updated

--- app/src/main/java/com/example/chandanaish/MainActivity.java
+++ app/src/main/java/com/example/chandanaish/MainActivity.java
@@ -6,15 +6,51 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.denzcoskun.imageslider.ImageSlider;
+import com.denzcoskun.imageslider.constants.ScaleTypes;
+import com.denzcoskun.imageslider.models.SlideModel;
+import com.etebarian.meowbottomnavigation.MeowBottomNavigation;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
     ImageView temparature;
+    TextView marq ,dateTime;
+    private MeowBottomNavigation meowBottomNavigation;
+    ImageSlider imageSlider;
+    ArrayList<SlideModel> imageList = new ArrayList<>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         temparature = findViewById(R.id.temparature);
+        marq = findViewById(R.id.marqueetext);
+        imageSlider =findViewById(R.id.image_slider);
+        dateTime= findViewById(R.id.dateandtime);
+        meowBottomNavigation=findViewById(R.id.bottomnavigation);
+
+        marq.setSelected(true);
+
+
+//slider image list
+        imageList.add(new SlideModel(R.drawable.ic_launcher_background,null));
+        imageList.add(new SlideModel(R.drawable.ic_launcher_background,null));
+        imageList.add(new SlideModel(R.drawable.ic_launcher_background,null));
+
+        imageSlider.setImageList(imageList);
+
+        //top bar date and time
+//        String currentDateTimeString = java.text.DateFormat.getDateTimeInstance().format(new Date());
+//        dateTime.setText(currentDateTimeString);
+meowBottomNavigation.add(new MeowBottomNavigation.Model(1,R.drawable.ic_baseline_home_24));
+        meowBottomNavigation.add(new MeowBottomNavigation.Model(2,R.drawable.ic_baseline_home_24));
 
         temparature.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -25,5 +61,6 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
     }
 }
